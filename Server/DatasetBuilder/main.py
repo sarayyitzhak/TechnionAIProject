d193a5d09d0131e1f67e693af0a0f0186fbfd326
Yakir Uzan <Uzan.yakir@gmail.com>
2023-03-16 19:16:54 +0200

    commit

--- Server/DatasetBuilder/main.py
+++ Server/DatasetBuilder/main.py
@@ -1,7 +1,6 @@
 from googleplaces import GooglePlaces, types, lang
-import mpu
 import json
-
+import decimal
 
 # 32.836823, 35.056948 - T
 # 32.758325, 35.022616 - B
@@ -10,48 +9,50 @@ import json
 
 # 32.797574, 35.015749 - C
 
-def main():
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+def get_google_api_key():
+    try:
+        with open('google-api-key.txt', 'r', encoding='utf-8') as f:
+            return f.read()
+    except IOError:
+        return None
+
+
+def get_raw_data(google_api_key: str):
     lat_lng = {
         "lat": 32.797574,
         "lng": 35.015749
     }
-    google_places = GooglePlaces('AIzaSyDgs_N1LazwhtigvfhsSQ_Qt6BaZwhvh6U')
-    qr = google_places.nearby_search(language=lang.HEBREW, lat_lng=lat_lng, radius=5820,
-                                     types=[types.TYPE_RESTAURANT, types.TYPE_CAFE])
+    google_places = GooglePlaces(google_api_key)
+    place_types = [types.TYPE_RESTAURANT, types.TYPE_CAFE]
+    qr = google_places.nearby_search(language=lang.HEBREW, lat_lng=lat_lng, radius=5820, types=place_types)
 
     data = []
     for i in qr.places:
         i.get_details()
         data.append(i.details)
-        print(i)
-        print('\n')
 
-    print("ASD")
-
-    # with open('data.json', 'w', encoding='utf-8') as f:
-    #     json.dump(data, f, ensure_ascii=False, indent=4)
-    # print (qr.places)
+    return data
 
 
-def test():
-    # Point one
-    lat1 = 32.836823
-    lon1 = 35.056948
+def write_to_file(data, file_name):
+    with open(file_name, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4, cls=DecimalEncoder)
 
-    # Point two
-    lat2 = 32.797574
-    lon2 = 35.015749
 
-    # What you were looking for
-    dist = mpu.haversine_distance((lat1, lon1), (lat2, lon2))
-    print(dist * 1000)
-
-    data = {
-        "dist": "שרי"
-    }
-
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+def main():
+    key = get_google_api_key()
+    if key is None:
+        print("ERROR")
+    else:
+        write_to_file(get_raw_data(key), "raw-data.json")
 
 
 if __name__ == '__main__':
