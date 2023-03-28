a2c558f237b74fd8fe223533e409511e56894dc7
Yakir Uzan <Uzan.yakir@gmail.com>
2023-03-29 01:55:32 +0300

    commit

--- Server/DataBuilder/GoogleDataBuilder.py
+++ Server/DataBuilder/GoogleDataBuilder.py
@@ -24,26 +24,31 @@ def get_raw_data_by_latitude(google_api_key: str, place_ids: set, location):
 
 
 def get_raw_data_by_coordinates(google_api_key: str, place_ids: set, lat_lng):
+    data = []
     google_places = GooglePlaces(google_api_key)
     place_types = [types.TYPE_RESTAURANT, types.TYPE_CAFE]
-    qr = google_places.nearby_search(language=lang.HEBREW, lat_lng=lat_lng, rankby=ranking.DISTANCE, types=place_types)
+    for place_type in place_types:
+        qr = google_places.nearby_search(language=lang.HEBREW, lat_lng=lat_lng, rankby=ranking.DISTANCE, types=[place_type])
 
-    data = []
-    for place in qr.places:
-        if place.place_id not in place_ids:
-            place.get_details()
-            data.append(place.details)
-            place_ids.add(place.place_id)
-
-    while qr.has_next_page_token:
-        time.sleep(5)   # Waiting for the next page to be ready
-        qr = google_places.nearby_search(language=lang.HEBREW, lat_lng=lat_lng, rankby=ranking.DISTANCE,
-                                         types=place_types, pagetoken=qr.next_page_token)
         for place in qr.places:
             if place.place_id not in place_ids:
+                if not place.rating:
+                    continue
                 place.get_details()
                 data.append(place.details)
                 place_ids.add(place.place_id)
+
+        while qr.has_next_page_token:
+            time.sleep(5)   # Waiting for the next page to be ready
+            qr = google_places.nearby_search(language=lang.HEBREW, lat_lng=lat_lng, rankby=ranking.DISTANCE,
+                                             types=place_types, pagetoken=qr.next_page_token)
+            for place in qr.places:
+                if place.place_id not in place_ids:
+                    if not place.rating:
+                        continue
+                    place.get_details()
+                    data.append(place.details)
+                    place_ids.add(place.place_id)
 
     return data
 
