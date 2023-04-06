64348b62d55efe0f70f45064a6f2f27be1d545ac
sarayakir <sarayakir19@gmail.com>
2023-04-06 23:53:42 +0300

    k

--- Server/main.py
+++ Server/main.py
@@ -1,6 +1,27 @@
+from Server.DataParser.DataParser import *
+from Server.DataBuilder.CbsDataBuilder import *
+from Server.DataBuilder.GovDataBuilder import *
+from Server.Algo.ID3 import *
+import pandas as pd
+import urllib.request
+import json
+import math
+import textdistance
 
 def main():
-    pass
+    # unneeded_labels = ["name", "user_ratings_total", "Unnamed: 0", "vicinity", "street", "city"]
+    # attributes_names = list(pd.read_csv("./Dataset/data.csv", delimiter=",", dtype=str, nrows=1).keys())
+    # for label in unneeded_labels:
+    #     attributes_names.remove(label)
+    # train_set = pd.read_csv("./Dataset/data.csv")
+    # train_set.drop(unneeded_labels, axis='columns', inplace=True)
+    # x_train = np.array(train_set.drop('rating', axis=1).copy())
+    # y_train = np.array(train_set['rating'].copy())
+    # id3 = ID3(attributes_names, 500)
+    # id3.fit(x_train, y_train)
+    x=0
+    # parse_data()
+    build_data_3()
 
 
 if __name__ == '__main__':
