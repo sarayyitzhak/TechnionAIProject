4a8df7155394fa795ea9a0d6c7fa1b98be8776be
sarayakir <sarayakir19@gmail.com>
2023-08-24 15:09:06 +0300

    prod client screens

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from Client.DevClient import *
+from PyQt5.QtWidgets import QApplication
+from Client.Screens.DevClientMainScreen import *
+from Client.ProdClientMainWindow import ProdClientMainWindow
 
 
 def main():
-    dev_client_app = QApplication([])
-    dev_client_app_window = DevClientMainWindow()
-    dev_client_app_window.show()
-    dev_client_app.exec()
+    ## DEV
+    # dev_client_app = QApplication([])
+    # dev_client_app_window = DevClientMainWindow()
+    # dev_client_app_window.show()
+    # dev_client_app.exec()
+    ## PROD
+    prod_client_app = QApplication([])
+    main_window = ProdClientMainWindow()
+    prod_client_app.exec()
 
 
 if __name__ == '__main__':
